badcbd2d524a5fc19190f88ebd8dad3d5de6391d
Kota Kobayashi <malgranda.arbaro@gmail.com>
2019-08-07 00:21:56 +0900

    Add web (initial state)

---
+++ web/src/test/kotlin/jp/ac/tut/img/oc2019/web/WebApplicationTests.kt
@@ -0,0 +1,16 @@
+package jp.ac.tut.img.oc2019.web
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
